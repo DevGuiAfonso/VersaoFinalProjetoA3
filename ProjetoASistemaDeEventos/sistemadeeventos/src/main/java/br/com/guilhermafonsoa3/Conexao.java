907f0a798977273ac7db2695109ac2da81b22530
DevGuiAfonso <dev.guilherme.afonso@gmail.com>
2025-06-11 15:45:50 -0300

    Update Conexao.java
    Coloque sua senha (MySQL) no local indicado

--- ProjetoASistemaDeEventos/sistemadeeventos/src/main/java/br/com/guilhermafonsoa3/Conexao.java
+++ ProjetoASistemaDeEventos/sistemadeeventos/src/main/java/br/com/guilhermafonsoa3/Conexao.java
@@ -8,7 +8,7 @@ public class Conexao {
     
     private static final String URL = "jdbc:mysql://localhost:3306/sistemadeeventos";
     private static final String USUARIO = "root";
-    private static final String SENHA = "@Guibela12";// Substitua pela sua senha do MySQL
+    private static final String SENHA = "";// Substitua pela sua senha do MySQL
 
     public static Connection obterConexao() throws SQLException{
         return DriverManager.getConnection(URL, USUARIO, SENHA);
